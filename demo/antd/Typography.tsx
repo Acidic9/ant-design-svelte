7248815018e61686fcd5d9d9650d65c4fc925baa
tommywalkie <tommywalkie@gmail.com>
2020-12-11 11:38:01 +0100

    Added TSX files and should fix #2

---
+++ demo/antd/Typography.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import { Typography } from 'antd'
+
+const { Title } = Typography
+
+export const Titles: FC = () => <div>
+    <Title>h1. Ant Design</Title>
+    <Title level={2}>h2. Ant Design</Title>
+    <Title level={3}>h3. Ant Design</Title>
+    <Title level={4}>h4. Ant Design</Title>
+    <Title level={5}>h5. Ant Design</Title>
+</div>
