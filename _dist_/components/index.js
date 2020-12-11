12e7935e93a1b906e04b0c0f7966928e406fa6d9
tommywalkie <tommywalkie@gmail.com>
2020-12-11 10:35:31 +0000

    Deploying to gh-pages from  @ 7248815018e61686fcd5d9d9650d65c4fc925baa 🚀

--- _dist_/components/index.js
+++ _dist_/components/index.js
@@ -2,6 +2,6 @@ export * from "./Grid/index.js";
 export * from "./Typography/index.js";
 export * from "./Layout/index.js";
 export * from "./Menu/index.js";
-export * from "./Card.js";
+export * from "./Card/index.js";
 export * from "./Tag/index.js";
 export {default as Button} from "./Button.js";
---
+++ antd/Typography.js
@@ -0,0 +1,12 @@
+import React from "https://cdn.skypack.dev/react";
+import {Typography} from "https://cdn.skypack.dev/antd";
+const {Title} = Typography;
+export const Titles = () => /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement(Title, null, "h1. Ant Design"), /* @__PURE__ */ React.createElement(Title, {
+  level: 2
+}, "h2. Ant Design"), /* @__PURE__ */ React.createElement(Title, {
+  level: 3
+}, "h3. Ant Design"), /* @__PURE__ */ React.createElement(Title, {
+  level: 4
+}, "h4. Ant Design"), /* @__PURE__ */ React.createElement(Title, {
+  level: 5
+}, "h5. Ant Design"));
